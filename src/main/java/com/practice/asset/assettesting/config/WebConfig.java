e72426a17408c8b1a38957f4e118f63cba5934b9
Suresh Babu Alahari <sureshbabualahari@MacBook-Air.local>
2024-11-03 17:50:28 -0500

    Initial Version

---
+++ src/main/java/com/practice/asset/assettesting/config/WebConfig.java
@@ -0,0 +1,20 @@
+package com.practice.asset.assettesting.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Allows all endpoints
+                .allowedOrigins("http://localhost:3001", "http://localhost:3002", "http://localhost:3000")
+//                .allowedOrigins("*") // Allow your front-end origin
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
+                .allowedHeaders("*") // Allow all headers
+                .allowCredentials(true); // Allow credentials if needed
+    }
+}
---
+++ src/test/java/com/practice/asset/assettesting/AssetTestingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.asset.assettesting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssetTestingApplicationTests {
+
+//	@Test
+	void contextLoads() {
+	}
+
+}
