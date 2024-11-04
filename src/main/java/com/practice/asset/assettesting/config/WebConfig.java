c2cfad56d0bfeaf58f3dc317b786ed5b28e5a5da
Suresh Babu Alahari <sureshbabualahari@MacBook-Air.local>
2024-11-04 14:39:32 -0500

    supporting all origins to support from UI

--- src/main/java/com/practice/asset/assettesting/config/WebConfig.java
+++ src/main/java/com/practice/asset/assettesting/config/WebConfig.java
@@ -11,8 +11,9 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allows all endpoints
-                .allowedOrigins("http://localhost:3001", "http://localhost:3002", "http://localhost:3000")
+//                .allowedOrigins("http://localhost:3001", "http://localhost:3002", "http://localhost:3000")
 //                .allowedOrigins("*") // Allow your front-end origin
+                .allowedOriginPatterns("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Specify allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
